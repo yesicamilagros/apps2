ebeaee5c60d25c9d9d3d2ca37637f62da1ebcf2c
yesicamilagros <milagroslc654@gmail.com>
2025-05-01 10:49:07 -0500

    primer commit2000

--- src/services/whatsappService.js
+++ src/services/whatsappService.js
@@ -6,9 +6,9 @@ class WhatsAppService {
     try {
       await axios({
         method: 'POST',
-        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+        url: `https://graph.facebook.com/${process.env.API_VERSION}/${process.env.BUSINESS_PHONE}/messages`,
         headers: {
-          Authorization: `Bearer ${config.API_TOKEN}`,
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
         },
         data: {
           messaging_product: 'whatsapp',
@@ -28,9 +28,9 @@ class WhatsAppService {
     try {
       await axios({
         method: 'POST',
-        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+        url: `https://graph.facebook.com/${process.env.API_VERSION}/${process.env.BUSINESS_PHONE}/messages`,
         headers: {
-          Authorization: `Bearer ${config.API_TOKEN}`,
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
         },
         data: {
           messaging_product: 'whatsapp',
@@ -47,9 +47,9 @@ class WhatsAppService {
     try {
       await axios({
         method: 'POST',
-        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+        url: `https://graph.facebook.com/${process.env.API_VERSION}/${process.env.BUSINESS_PHONE}/messages`,
         headers: {
-          Authorization: `Bearer ${config.API_TOKEN}`,
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
         },
         data: {
           messaging_product: 'whatsapp',
@@ -92,9 +92,9 @@ class WhatsAppService {
 
       await axios({
         method: 'POST',
-        url: `https://graph.facebook.com/${config.API_VERSION}/${config.BUSINESS_PHONE}/messages`,
+        url: `https://graph.facebook.com/${process.env.API_VERSION}/${process.env.BUSINESS_PHONE}/messages`,
         headers: {
-          Authorization: `Bearer ${config.API_TOKEN}`,
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
         },
         data: {
           messaging_product: 'whatsapp',
