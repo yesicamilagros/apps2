59c270f181b7a5c3f2d5901a25f9066a9aa23430
yesicamilagros <milagroslc654@gmail.com>
2025-05-01 10:38:01 -0500

    primer commit1999

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-    WEEKHOOK_VERIFY_TOKEN: process.env.WEBHOOK_VERIFY_TOKEN,
+  WEBHOOK_VERIFY_TOKEN: process.env.WEBHOOK_VERIFY_TOKEN,
     API_TOKEN: process.env.API_TOKEN,
     BUSINESS_PHONE: process.env.BUSINESS_PHONE,
     API_VERSION: process.env.API_VERSION,
